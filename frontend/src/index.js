5e8325c76c11eea1c5284fa35aa0e406ff5700e4
Connor Smith <connor@sphinx.io>
2020-06-30 15:54:24 -0400

    cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import JavascriptTimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+
 import "./tailwind.generated.css";
 import App from "./components/App";
 import AppStateProvider from "./state";
 import "./index.css";
-import JavascriptTimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en";
 
 JavascriptTimeAgo.addLocale(en);
 
--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,28 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: [],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
-    },
-    fontSize: {
-      xs: '0.75rem',
-      sm: '0.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '3.5rem',
     },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/ui')
-  ]
-}
+  plugins: [require("@tailwindcss/ui")],
+};
