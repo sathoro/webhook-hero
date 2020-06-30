d71ef532b0d160d5d68b6fb77975108d9a1ce248
Connor Smith <connor@sphinx.io>
2020-06-30 15:56:43 -0400

    adds breadcrumbs

--- frontend/src/components/CreateWebhook.js
+++ frontend/src/components/CreateWebhook.js
@@ -8,7 +8,6 @@ import { useHistory } from "react-router-dom";
 export default function CreateWebhook() {
   const history = useHistory();
   const [errors, setErrors] = useState({});
-
   const nameInput = useRef();
 
   const createWebhook = (event) => {
@@ -21,45 +20,57 @@ export default function CreateWebhook() {
           name: "Default",
         });
       })
-      .then(() => history.push("/webhooks"))
+      .then(() => history.push("/webhooks", { created: true }))
       .catch(setErrors);
   };
 
   return (
     <>
-      <PageHeading name="Create Webhook" />
+      <PageHeading
+        breadcrumbs={[
+          {
+            to: "/webhooks",
+            name: "Webhooks",
+          },
+          {
+            to: `/webhooks/create`,
+            name: "Create Webhook",
+          },
+        ]}
+        name="Create Webhook"
+      />
 
       <main>
-        <div class="max-w-3xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div class="mt-5 md:mt-0 md:col-span-2">
+        <div className="max-w-3xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="mt-5 md:mt-0 md:col-span-2">
             <form onSubmit={createWebhook}>
-              <div class="shadow sm:rounded-md sm:overflow-hidden">
-                <div class="px-4 py-5 bg-white sm:p-6">
+              <div className="shadow sm:rounded-md sm:overflow-hidden">
+                <div className="px-4 py-5 bg-white sm:p-6">
                   <Error error={errors} />
 
                   <div>
                     <label
-                      for="name"
-                      class="block text-sm font-medium leading-5 text-gray-700"
+                      htmlFor="name"
+                      className="block text-sm font-medium leading-5 text-gray-700"
                     >
                       Webhook Name
                     </label>
-                    <div class="mt-1 flex rounded-md shadow-sm">
+                    <div className="mt-1 flex rounded-md shadow-sm">
                       <input
                         ref={nameInput}
                         id="name"
                         type="text"
                         required
-                        class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                       />
                     </div>
                   </div>
                 </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <span class="inline-flex rounded-md shadow-sm">
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <span className="inline-flex rounded-md shadow-sm">
                     <button
                       type="submit"
-                      class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                      className="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                     >
                       Save
                     </button>
--- frontend/src/components/PageHeading.js
+++ frontend/src/components/PageHeading.js
@@ -1,21 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function PageHeading({ name, buttons }) {
+export default function PageHeading({ name, buttons, breadcrumbs }) {
   return (
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <div class="md:flex md:items-center md:justify-between">
-          <div class="flex-1 min-w-0">
-            <h1 class="text-3xl font-semibold leading-tight text-gray-900">
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        {(breadcrumbs || []).length ? (
+          <nav class="hidden sm:flex items-center text-sm leading-5 font-medium mb-2">
+            {breadcrumbs.map((breadcrumb, i) => (
+              <span key={i} class="sm:flex">
+                <Link
+                  to={breadcrumb.to}
+                  className="text-gray-500 hover:text-gray-700 transition duration-150 ease-in-out"
+                >
+                  {breadcrumb.name}
+                </Link>
+                {i < breadcrumbs.length - 1 && (
+                  <svg
+                    class="flex-shrink-0 mx-2 h-5 w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+              </span>
+            ))}
+          </nav>
+        ) : null}
+
+        <div className="md:flex md:items-center md:justify-between">
+          <div className="flex-1 min-w-0">
+            <h1 className="text-3xl font-semibold leading-tight text-gray-900">
               {name}
             </h1>
           </div>
-          <div class="mt-3 flex md:mt-0 md:ml-4">
-            <span class="ml-3 shadow-sm rounded-md">
-              {(buttons || []).map((btn) => (
+          <div className="mt-3 flex md:mt-0 md:ml-4">
+            <span className="ml-3 shadow-sm rounded-md">
+              {(buttons || []).map((btn, i) => (
                 <button
+                  key={i}
                   type="button"
-                  class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
                   onClick={btn.onClick}
                 >
                   {btn.name}
