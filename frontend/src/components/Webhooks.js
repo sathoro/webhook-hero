2bd96dc8c30641e5931973155d85aa84e6ef34b6
Connor Smith <connor@sphinx.io>
2020-06-30 15:58:29 -0400

    adds Alert to Webhooks component

--- frontend/src/components/Webhooks.js
+++ frontend/src/components/Webhooks.js
@@ -4,11 +4,12 @@ import Api from "../utils/Api";
 import Webhook from "./Webhook";
 import PageHeading from "./PageHeading";
 import Alert from "./Alert";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
-export default function Webhooks() {
+export default () => {
   const [webhooks, setWebhooks] = useState(null);
   const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
     Api.get("/api/v1/webhook").then(setWebhooks);
@@ -27,18 +28,32 @@ export default function Webhooks() {
       />
 
       {webhooks && !webhooks.length ? (
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <div class="font-bold text-xl mb-5">No webhooks created yet.</div>
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <div className="font-bold text-xl mb-5">No webhooks created yet.</div>
         </div>
       ) : null}
 
-      {/* <div class="max-w-3xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <Alert type="success" title="Webhook created" body={<p>Test</p>} />
-      </div> */}
+      {location.state &&
+        (location.state.created ||
+          location.state.updated ||
+          location.state.deleted) && (
+          <div className="max-w-3xl mx-auto py-6 px-4 sm:px-6 lg:px-8 -mb-6">
+            <Alert
+              type="success"
+              title={`Webhook ${
+                location.state.created
+                  ? "created"
+                  : location.state.updated
+                  ? "updated"
+                  : "deleted"
+              }`}
+            />
+          </div>
+        )}
 
       {(webhooks || []).map((webhook) => (
         <Webhook key={webhook.id} webhook={webhook} />
       ))}
     </>
   );
-}
+};
